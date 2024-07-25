fcd546d684c42d795289ce4958f71db3849c9db6
Sebastientg <146782079+Sebastientg@users.noreply.github.com>
2024-07-25 11:36:02 -0700

    Update uncompress.cpp

--- uncompress.cpp
+++ uncompress.cpp
@@ -3,48 +3,51 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    // Check for correct arguments
+    // Check for correct number of arguments
     if (argc != 3) {
         error("Usage: uncompress <input_file> <output_file>");
     }
 
-    // Open input file
+    // Open input file using a custom input stream class
     FancyInputStream input(argv[1]);
 
-    // Handle empty file
+    // Handle the case where the input file is empty
     if (input.filesize() == 0) {
+        // Open output file using a custom output stream class
         FancyOutputStream output(argv[2]);
         return 0; 
     }
 
-    // Read the number of unique symbols from the header
+    // Read the number of unique symbols from the input file header
     int numSymbols = input.read_int();
 
-    // Read symbol frequencies from the header (optimized)
-    vector<int> freqs(256, 0);
+    // Read the symbol frequencies from the input file header in an optimized format
+    vector<int> freqs(256, 0);  // Vector to store frequencies of each byte (0-255)
     for (int i = 0; i < numSymbols; ++i) {
-        unsigned char symbol = input.read_byte();
+        unsigned char symbol = input.read_byte();  // Read the symbol
+        // Read the frequency of the symbol using 3 bytes (big-endian format)
         int count = (input.read_byte() << 16) | (input.read_byte() << 8) | input.read_byte();
-        freqs[symbol] = count;
+        freqs[symbol] = count;  // Store the frequency in the vector
     }
 
-    // Build Huffman tree with the given frequencies
+    // Build the Huffman tree using the frequencies of each byte
     HCTree tree;
     tree.build(freqs);
 
-    // Open output file
+    // Open output file for writing the decompressed data
     FancyOutputStream output(argv[2]);
 
-    // Decode bits from input file into sequence of bytes and write to output file
-    int counter = 0;
+    // Decode bits from the input file into a sequence of bytes and write them to the output file
+    int counter = 0;  // Variable to count the total number of bytes to decode
     for (int freq : freqs) {
-        counter += freq;
-    }
-    while (counter != 0) {
-        unsigned char symbol = tree.decode(input);
-        output.write_byte(symbol);
-        --counter; 
+        counter += freq;  // Sum up the frequencies to get the total number of bytes
     }
 
-    return 0;
+    while (counter != 0) {
+        unsigned char symbol = tree.decode(input);  // Decode the next symbol using the Huffman tree
+        output.write_byte(symbol);  // Write the decoded symbol to the output file
+        --counter;  // Decrement the counter
+    }
+
+    return 0;  // Return 0 to indicate successful execution
 }
